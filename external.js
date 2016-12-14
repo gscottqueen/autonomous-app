fe61d39b348977e67383413d17a633d94f99aebe
G. Scott Queen <geoffreysqueen@gmail.com>
2016-12-14 10:58:56 -0500

    null variable defined

--- external.js
+++ external.js
@@ -1,31 +1,32 @@
 $(document).ready(function(){
-  $("#report-incident").submit(function(e){
-    e.preventDefault()
-    var month = $("#month_id").val()
-    var day = $("#day").val()
-    console.log(day);
-    writeData(incident_id, month, day, description)
+  // $("#report-incident").submit(function(e){
+  //   e.preventDefault()
+  //   var month = $("#month_id").val()
+  //   var day = $("#day").val()
+  //   console.log(day);
+  //   writeData(incident_id, month, day, description)
   })
 
+// call to the database
+
+var dataset = [];
+
  firebase.database().ref('/incidents').once('value').then(function(snapshot) {
-    var dataset = snapshot.val();
-    var dataset = [{
-      x: incidents.month_id,
-      y: incidents.day
-    }];
-    console.log(incidents);
+    dataset = snapshot.val();
+    console.log(dataset);
   });
+
 
 // on click of submit call the writeData function
 // get the variables from the form
 // pass the variables into the set object for the firebase.database
 
-  function writeData(incident_id, month, day, description) {
-    firebase.database().ref('incidents/' + incident_id).set({
-      username: email,
-      month_id: month,
-      day: day,
-      description: description
-    });
-  }
-})
+//   function writeData(incident_id, month, day, description) {
+//     firebase.database().ref('incidents/' + incident_id).set({
+//       username: email,
+//       month_id: month,
+//       day: day,
+//       description: description
+//     });
+//   }
+// })
